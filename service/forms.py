2f618ba85f52dcb843787f2eea3627ef49028654
ybrbnf <ybrbnf418@gmail.com>
2019-09-19 14:09:42 +0400

    upd AddForm

--- service/forms.py
+++ service/forms.py
@@ -2,13 +2,19 @@ from django import forms
 from .models import Services
 
 
-class AddForm(forms.ModelForm):
+class AddForm(forms.ModelForm):	
+    def __init__(self, *args, **kwargs):
+        super(AddForm, self).__init__(*args, **kwargs)
+        self.fields['cold_water'].initial = '0'
+        self.fields['hot_water'].initial = '0'
+
 
     class Meta:
         model = Services
-        fields = ('cold_water', 'hot_water', 'electricity', 'gaz',)
+        fields = ('cold_water','hot_water', 'electricity', 'gaz',)
+
         labels = {'cold_water': ('Холодная вода'),
-                  'hot_water': ('Горячая вода'),
+        		  'hot_water': ('Горячая вода'),
                   'electricity': ('Электричество'),
                   'gaz': ('Газ'),
                   }
